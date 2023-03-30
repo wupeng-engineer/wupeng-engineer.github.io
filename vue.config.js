246e891513c0ba323def5e636f0339298b213b4a
wupeng <39206058+wupeng-engineer@users.noreply.github.com>
2023-03-30 13:35:39 +0800

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const path = require('path')
 const UglifyPlugin = require('uglifyjs-webpack-plugin')
 
 module.exports = {
-    publicPath: './yidie.github.io/',
+    publicPath: '/yidie.github.io/',
     assetsDir: 'static',
     lintOnSave: false,
     chainWebpack: (config) => {},
